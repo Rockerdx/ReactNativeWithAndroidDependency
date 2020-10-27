3100698ceda42f86d1a569a413b874d9cf92bf5d
Rizky <rizky.putra@gmail.com>
2020-10-27 22:46:26 +0700

    test

--- android/app/src/main/java/com/awesomeproject/CalculationModule.java
+++ android/app/src/main/java/com/awesomeproject/CalculationModule.java
@@ -1,13 +1,47 @@
 package com.awesomeproject;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 
 import com.example.mylibrary.Calculation;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-public class CalculationModule extends ReactContextBaseJavaModule {
+public class CalculationModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+
+    private ReactApplicationContext mContext;
+
+    private final BroadcastReceiver mHeadsetPlugReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
+                boolean plugged = (intent.getIntExtra("state", 0) == 1);
+                String message = plugged ? "Headset plugged in" : "Headset plugged out";
+                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    public CalculationModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        mContext = reactContext;
+        registerBroadcastReceiver();
+    }
+
+    private void registerBroadcastReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_HEADSET_PLUG);
+        mContext.registerReceiver(mHeadsetPlugReceiver, filter);
+    }
+
 
     @NonNull
     @Override
@@ -18,5 +52,25 @@ public class CalculationModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void addNumber(int a, int b, Promise promise){
         promise.resolve(new Calculation().addNumber(a,b));
+    }
+
+    @ReactMethod
+    public void startTrackingAudioJackPlug() {
+        registerBroadcastReceiver();
+    }
+
+    @Override
+    public void onHostResume() {
+
+    }
+
+    @Override
+    public void onHostPause() {
+
+    }
+
+    @Override
+    public void onHostDestroy() {
+        mContext.unregisterReceiver(mHeadsetPlugReceiver);
     }
 }
